3b5f41e42221396007dbfbc3e8e5e2e8ef7cc9ea
staszko24 <161465380+staszko24@users.noreply.github.com>
2024-11-03 11:05:17 +0100

    Update nestedArrays.js

--- nestedArrays.js
+++ nestedArrays.js
@@ -1,10 +1,10 @@
-let fruits = ['Apple','Lemon',['Banana', 'Mango'],['Strawberry']];
+const fruits = ['Apple','Lemon',['Banana', 'Mango'],['Strawberry']];
 
 function findFruit(fruit, array) {
     for (let i = 0; i < array.length; i++) {
         const e = array[i];
         if (e.includes(fruit)) {
-           return `${fruit} is in ${i}.`;
+           return `${fruit} is in Fruits.`;
         }   
     }
     return `${fruit} is not in the Fruits.`;
@@ -15,6 +15,6 @@ console.log(findFruit('Melon', fruits));
 console.log(findFruit('Strawberry', fruits));
 console.log(findFruit('Lemon', fruits));
 console.log(findFruit('Mango', fruits));
-alert(findFruit('Lemon',fruits));
+alert(findFruit('Orange',fruits));
 
 
